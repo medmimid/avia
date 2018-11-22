25faf6840e452e6f4b81b7eb02d78619bdf49192
anantanant2015 <anantanant2015@gmail.com>
2018-11-22 11:26:03 +0530

    Redirect to dashboard from base url.(#325)
    admin_app
    ---------
    
    - Updates page controller to redirect to dashboard page.

--- apps/admin_app/lib/admin_app_web/controllers/page_controller.ex
+++ apps/admin_app/lib/admin_app_web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule AdminAppWeb.PageController do
   use AdminAppWeb, :controller
 
+  alias AdminAppWeb.Helpers
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> redirect(
+      to:
+        dashboard_path(conn, :index, %{
+          "from" => Helpers.date_days_before(30),
+          "to" => Helpers.date_today()
+        })
+    )
   end
 end
